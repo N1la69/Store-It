d389a21ed6d502d880bb3073f60e9b194995d081
Nilanjan Saha <nilanjan.s.1920@gmail.com>
2024-11-21 20:28:33 +0530

    auth_layout

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,50 @@
+import Image from "next/image";
+import React from "react";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex min-h-screen">
+      <section className="bg-brand p-10 hidden w-1/2 items-center justify-center lg:flex xl:w-2/5">
+        <div className="flex max-h-[800px] max-w-[430px] flex-col justify-center space-y-12">
+          <Image
+            src="/logo.svg"
+            width={224}
+            height={82}
+            alt="logo"
+            className="h-auto"
+          />
+          <div className="space-y-5 text-white">
+            <h1 className="h1">Manage your files the best way</h1>
+            <p className="body-1">
+              This is where you can store all your documents and files in one
+              place.
+            </p>
+          </div>
+
+          <Image
+            src="/illustration.png"
+            alt="illustration"
+            width={342}
+            height={342}
+            className="transition-all hover:scale-105 hover:rotate-2"
+          />
+        </div>
+      </section>
+
+      <section className="flex flex-1 flex-col items-center bg-white p-4 py-10 lg:justify-center lg:p-10 lg:py-0">
+        <div className="mb-16 lg:hidden">
+          <Image
+            src="/assets/icons/logo-full-brand.svg"
+            alt="logo"
+            width={224}
+            height={82}
+            className="h-auto w-[200px] lg:w-[250px]"
+          />
+        </div>
+        {children}
+      </section>
+    </div>
+  );
+};
+
+export default Layout;
